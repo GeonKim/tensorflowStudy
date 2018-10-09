adb27530c803ba2e20b74a16199e822c41664128
GeonKim <ghk9204@gmail.com>
2018-10-09 15:22:39 +0900

    none

--- lab02-2.py
+++ lab02-2.py
@@ -1,14 +1,30 @@
 import tensorflow as tf
 
 #Placeholder 버젼
-print("\n\nPlaceHolder Example")
 
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
 
-h = tf.add(tf.matmul(W,X), b)
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
 
-cost_val = tf.reduce_mean(tf.square(h - Y))
+hypothesis = tf.add(tf.multiply(W,X), b)
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
 
 for step in range(2001):
+    cost_val, w_val, b_val, _ = sess.run([cost, W, b, train], feed_dict={X:[1, 2, 3, 4, 5], Y:[2.1, 3.1, 4.1, 5.1, 6.1]})
+    if step % 100 == 0:
+        print("step:{}\tcost:{:.5f}\tW:{}\tb:{}".format(step, cost_val, w_val, b_val))
 
+
+#testing our model
+print(sess.run(hypothesis, feed_dict={X: [5]}))
+print(sess.run(hypothesis, feed_dict={X: [2.5]}))
+print(sess.run(hypothesis, feed_dict={X: [1.5, 3.5]}))
